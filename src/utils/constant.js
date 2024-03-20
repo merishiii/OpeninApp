2d6a4cb523a42d5e8bedd22b75b65829e82bb827
Rishabh Pathak <99328708+merishiii@users.noreply.github.com>
2024-03-20 14:27:26 +0530

    Updating

--- src/utils/constant.js
+++
@@ -1,7 +0,0 @@
-// export const DB_CONNECT="mongodb+srv://pathakrishabh620:QIHJ6pNnKWv8Mo9N@cluster0.rbtpypz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-export const DB_CONNECT="mongodb+srv://pathakrishabh620:QIHJ6pNnKWv8Mo9N@cluster0.rbtpypz.mongodb.net/todo?retryWrites=true&w=majority&appName=Cluster0"
-export const StatusCode={
-    SUCCESS:200,
-    VALIDATION_ERROR:201,
-}
--- src/utils/helpers.js
+++
@@ -1,3 +0,0 @@
-export const jsonGenrate=(statusCode,message,data)=>{
-    return {status: statusCode,message:message,data:data}
-}
